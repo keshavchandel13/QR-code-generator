dee515b5746f52d30151e1dd9230a35a373629ba
Keshav Chandel <chandelkeshav4@gmail.com>
2024-07-07 21:52:19 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+const btn=document.getElementById("btn");
+
+
+btn.addEventListener('click',()=>{
+    const text=document.getElementById("input-area").value;
+    if(text.value==null){
+        window.alert("Enter valid text");
+    }
+    else{
+    document.getElementById("img").src=`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${text}`
+   }
+});
